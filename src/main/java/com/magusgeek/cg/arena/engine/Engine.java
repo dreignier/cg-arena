d79b66b2bc9727c5594cba904a5692d1b0571ee1
David Reignier <david.reignier@gmail.com>
2015-11-11 14:45:57 +0100

    Add basic classes and configuration for Tron Engine.

---
+++ src/main/java/com/magusgeek/cg/arena/engine/Engine.java
@@ -0,0 +1,12 @@
+package com.magusgeek.cg.arena.engine;
+
+import java.util.List;
+
+public class Engine {
+
+    private List<Process> process;
+
+    public Engine(List<Process> process) {
+        this.process = process;
+    }
+}
---
+++ src/main/java/com/magusgeek/cg/arena/engine/Tron.java
@@ -0,0 +1,11 @@
+package com.magusgeek.cg.arena.engine;
+
+import java.util.List;
+
+public class Tron extends Engine {
+
+    public Tron(List<Process> process) {
+        super(process);
+    }
+
+}
