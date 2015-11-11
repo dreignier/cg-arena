d8436ca42c586a9648d580c29fbe28fa68269ea7
David Reignier <david.reignier@gmail.com>
2015-11-11 14:45:42 +0100

    The Arena now parse the command line and initialize the loggers correctly.

--- src/main/java/com/magusgeek/cg/arena/Arena.java
+++ src/main/java/com/magusgeek/cg/arena/Arena.java
@@ -1,9 +1,83 @@
 package com.magusgeek.cg.arena;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.config.Configurator;
+
 public class Arena {
 
-	public static void main(String[] args) {
-		
-	}
-	
+    private static final Log LOG = LogFactory.getLog(Arena.class);
+
+    public static void main(String[] args) {
+        try {
+            Options options = new Options();
+
+            options.addOption("h", false, "Print the help");
+            options.addOption("v", false, "If given, cg-arena will be very talkative");
+            options.addOption("e", true, "The engine to use");
+            options.addOption("p1", true, "Command line for player 1 program");
+            options.addOption("p2", true, "Command line for player 2 program");
+            options.addOption("p3", true, "Command line for player 3 program");
+            options.addOption("p4", true, "Command line for player 4 program");
+
+            CommandLine cmd = new DefaultParser().parse(options, args);
+            
+            if (cmd.hasOption("h")) {
+                new HelpFormatter().printHelp("-e <engine> -p1 <player1 command line> -p2 <player2 command line> -p3 <player3 command line> -p4 <player4 command line> [-v]", options);
+                System.exit(0);
+            }
+
+            boolean verbose = cmd.hasOption("v");
+            if (verbose) {
+                Configurator.setRootLevel(Level.ALL);
+                LOG.info("Verbose mode activated");
+            }
+
+            // Retrieve the engine
+            String engineName = cmd.getOptionValue("e");
+            if (engineName == null) {
+                LOG.fatal("An engine must be provided. Use -e to give it");
+                System.exit(1);
+            }
+            
+            Properties engines = new Properties();
+            engines.load(Arena.class.getClassLoader().getResourceAsStream("com/magusgeek/cg/arena/engines.properties"));
+            
+            if (!engines.containsKey(engineName)) {
+                LOG.fatal("Unrecognized engine name : " + engineName);
+                LOG.fatal("Possible values :");
+                
+                for (Object key : engines.keySet()) {
+                    LOG.fatal("- " + key);
+                }
+            }
+
+            List<String> playersCommandLines = new ArrayList<>();
+            for (int i = 1; i < 5; ++i) {
+                String p = cmd.getOptionValue("p" + i);
+
+                if (p != null) {
+                    playersCommandLines.add(p);
+                }
+            }
+            
+            if (playersCommandLines.size() < 2) {
+                LOG.fatal("At least 2 players are required, only " + playersCommandLines.size() + " given");
+                System.exit(1);
+            }
+        } catch (Exception exception) {
+            LOG.fatal("cg-arena fail to start", exception);
+            System.exit(1);
+        }
+    }
+
 }
